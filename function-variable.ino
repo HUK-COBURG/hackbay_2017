94f0bb86515d057a25a26ad7e59a5ea9601ce008
Christian Häussler <c-n-i@web.de>
2017-05-18 09:55:08 +0200

    Update function-variable.ino

--- function-variable.ino
+++ function-variable.ino
@@ -8,18 +8,22 @@
 
 // Include der MQTT Library zum Sendnen der Daten an die RaspberryPi Zentrale
 #include <MQTT.h>
+// Include der DHT Library zum Messen von Temperatur und Luftfeuchte
+#include <Adafruit_DHT.h>
 
 // Deklaration der Pins, die als Eingänge und Ausgänge benutzt werden
 int watermeter = A0; // Simulation für Wasserstand
 int bewegung = D0; // Simulation für Einbruch
 int relais = D5; // Simulation für Steckdose
 int wind = A1; // Simulation für Windstärke
+#define DHTPIN 2 // Pin zum DHT Sensor
 
 // Deklaration der Variablen für MQTT und der Particle-Cloud
 int wasserstand; // Höhe des Wasserstandes
 int bewegungsst; // Status des Bewegungsmelders
 int windstaerke; // Windstärke in km/h
 int relaisst; // Relais-Status
+#define DHTTYPE DHT11 // Type des DHT Sensors: DHT 11
 
 // Konstant-Variable zur Berechnung des Windstärke
 float voltageConversionWindConstant = .004882814;
@@ -33,6 +37,9 @@ void callback(char* topic, byte* payload, unsigned int length) {
 
 // MQTT Client zum sende an die RaspberryPi Zentrale
 MQTT client("172.17.100.252", 1883, callback);
+
+// DHT Sensor
+DHT dht(DHTPIN, DHTTYPE);
 
 // Initiales Setup der Funktionen
 void setup() {
@@ -53,8 +60,11 @@ void setup() {
     Particle.function("relais", relaisToggle);
 
     // Verbindung zum MQTT Server aufbauen (RaspberryPi Zentrale)
-    client.connect("Smart-E1");
+    client.connect("smart-e1");
 
+    // DHT Sensor initialisieen
+    dht.begin();
+    delay(10000); // 10 Sekunden warten
 }
 
 // Zentrale Loop Funktion...
@@ -65,11 +75,18 @@ void loop() {
     bewegungsst = digitalRead(bewegung);
     windstaerke = analogRead(wind) * voltageConversionWindConstant;
 
+    // Luftfeuchte vom DHT lesen
+    float h = dht.getHumidity();
+    // Temperatur vom DHT lesen
+    float t = dht.getTempCelcius();
+    
     // Variablen in die Particle Cloud publishen
     Particle.publish("Wasser-Sensor", String(wasserstand), 10);
     Particle.publish("Bewegungs-Status", String(bewegungsst), 10);
     Particle.publish("Wind-Stärke", String(windstaerke), 10);
     Particle.publish("Relais-Status", String(relaisst), 10);
+    Particle.publish("Luftfeuchte", String(h), 10);
+    Particle.publish("Temperatur", String(t), 10);
     
     // Senden der MQTT Messages
     if (client.isConnected()) {
@@ -77,6 +94,8 @@ void loop() {
         client.publish("smart-e1/bewegung", String(bewegungsst));
         client.publish("smart-e1/windstaerke", String(windstaerke));
         client.publish("smart-e1/relais", String(relaisst));
+        client.publish("smart-e1/luftfeuchte", String(h));
+        client.publish("smart-e1/temperatur", String(t));
     }
     
     // Warteschleife
